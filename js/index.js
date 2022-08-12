ae834a135dffda7eaa79b173d4388d37ab236b6e
sanjay <sanjaykumar.pushadapu@gmail.com>
2022-08-12 17:57:06 +0530

    Shuffle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 
-let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //page load arrange numbers in asc order
 window.onload = () => {
   onSort();
@@ -8,19 +7,22 @@ window.onload = () => {
 
 // button click arrange numbers in asc order
 const onSort = () => {
-  numberList = numberList.sort();
-  arrangeItems();
+  arrangeItems(numberList.sort());
 };
 
 // button click Shuffle numbers in radom order
 const onShuffle = () => {
-  numberList = numberList.sort(() => Math.random() - 0.9);
-  arrangeItems();
+  let arr=[...numberList];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  arrangeItems(arr);
 };
 
 //updatinging dom based on number index
-const arrangeItems = () => {
-  numberList.forEach((item, index) => {
+const arrangeItems = (data) => {
+  data.forEach((item, index) => {
     let ele = document.getElementById("boxId-" + (index + 1));
     if (ele) {
       ele.innerHTML = item;
